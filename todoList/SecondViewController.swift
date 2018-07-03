a2ec5463c348dfee58dfecf576dac4e763226571
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-07-02 23:57:25 -0400

    array created

--- todoList/SecondViewController.swift
+++ todoList/SecondViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var itemField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addItem(_ sender: Any) {
+        let itemsObj = UserDefaults.standard.object(forKey: "items")
+        
+        if let items = itemsObj as? NSMutableArray {
+            items.addObjects(from: [itemField.text as Any])
+        } else {
+            var items = [itemField.text]
+        }
+    }
+    
 }
 
