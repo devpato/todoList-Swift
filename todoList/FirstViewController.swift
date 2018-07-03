e1fb40e3c2babd76009bdcc6fa161f4b1b52a2e0
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-07-02 21:58:45 -0400

    initial setup

---
+++ todoList/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  todoList
+//
+//  Created by Patricio Vargas on 7/2/18.
+//  Copyright © 2018 Patricio Vargas. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
