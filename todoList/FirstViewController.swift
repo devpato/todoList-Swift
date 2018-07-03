342226ffe2e3f19a02d48dccb2485c1c3f539cf1
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-07-02 22:08:28 -0400

    table added and main cell

--- todoList/FirstViewController.swift
+++ todoList/FirstViewController.swift
@@ -8,7 +8,18 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "Test"
+        
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
